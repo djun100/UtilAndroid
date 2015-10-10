ba474b8257892da7e65128259dc6fa47ffab150d
djun100 <djun100@qq.com>
2015-10-10 10:32:55 +0800

    增加UtilArrayList工具

---
+++ utilAndroid/src/main/java/com/cy/DataStructure/UtilArrayList.java
@@ -0,0 +1,9 @@
+package com.cy.DataStructure;
+
+import java.util.ArrayList;
+
+public class UtilArrayList<E> extends ArrayList<E> {
+        public UtilArrayList<E> addOne(E e){
+            return this;
+        }
+    }
---
+++ utilAndroid/src/main/java/com/cy/DataStructure/UtilList.java
@@ -0,0 +1,10 @@
+package com.cy.DataStructure;
+
+import java.util.ArrayList;
+
+/**
+ * Created by cy on 2015/10/10.
+ */
+public class UtilList<E> {
+
+}
