017c8fadeaae1574cf799274fb4e3fb5f2203a62
djun100 <djun100@qq.com>
2016-04-05 14:17:49 +0800

    增加复制文件夹功能

--- utilAndroid/src/main/java/com/cy/File/UtilFile.java
+++ utilAndroid/src/main/java/com/cy/File/UtilFile.java
@@ -16,6 +16,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
@@ -151,7 +152,44 @@ public class UtilFile {
             return false;
         }
     }
+    /**
+     * 覆盖方式复制一个目录及其子目录、文件到另外一个目录
+     * @param src
+     * @param dest
+     * @throws IOException
+     * PS： apache commons-io包，FileUtils有相关的方法，IOUtils一般是拷贝文件。
 
+    删除目录结构                    FileUtils.deleteDirectory(dest);
+    递归复制目录及文件        FileUtils.copyDirectory(src, dest);
+     */
+    public static void copyFolder(File src, File dest) throws IOException {
+
+        if (src.isDirectory()) {
+            if (!dest.exists()) {
+                dest.mkdir();
+            }
+            String files[] = src.list();
+            for (String file : files) {
+                File srcFile = new File(src, file);
+                File destFile = new File(dest, file);
+                // 递归复制
+                copyFolder(srcFile, destFile);
+            }
+        } else {
+            InputStream in = new FileInputStream(src);
+            OutputStream out = new FileOutputStream(dest);
+
+            byte[] buffer = new byte[1024];
+
+            int length;
+
+            while ((length = in.read(buffer)) > 0) {
+                out.write(buffer, 0, length);
+            }
+            in.close();
+            out.close();
+        }
+    }
     /**
      * 智能添加路径开头分隔符
      *
