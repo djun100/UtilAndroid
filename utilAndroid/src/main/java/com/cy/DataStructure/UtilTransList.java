ea88a21d32ca108e5879eae09f54f4d5b4cc700a
djun100 <djun100@qq.com>
2016-08-18 10:07:12 +0800

    a

--- utilAndroid/src/main/java/com/cy/DataStructure/UtilTransList.java
+++
@@ -1,8 +0,0 @@
-package com.cy.DataStructure;
-
-import java.util.ArrayList;
-
-public class UtilTransList {
-
-}
- 
---
+++ utilAndroid/src/main/java/com/cy/DataStructure/UtilUrl.java
@@ -0,0 +1,97 @@
+package com.cy.DataStructure;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2016/8/18.
+ */
+public class UtilUrl {
+    /**
+     * 解析出url参数中的键值对
+     * 如 "index.jsp?Action=del&id=123"，解析出Action:del,id:123存入map中
+     *
+     * @param url url地址
+     * @return url请求参数部分
+     */
+    public static Map<String, String> extractParam(String url) {
+        Map<String, String> mapRequest = new HashMap<String, String>();
+
+        String[] arrSplit = null;
+
+        String strUrlParam = TruncateUrlPage(url);
+        if (strUrlParam == null) {
+            return mapRequest;
+        }
+        //每个键值为一组
+        arrSplit = strUrlParam.split("[&]");
+        for (String strSplit : arrSplit) {
+            String[] arrSplitEqual = null;
+            arrSplitEqual = strSplit.split("[=]");
+
+            //解析出键值
+            if (arrSplitEqual.length > 1) {
+                //正确解析
+                mapRequest.put(arrSplitEqual[0], arrSplitEqual[1]);
+
+            } else {
+                if (arrSplitEqual[0] != "") {
+                    //只有参数没有值，不加入
+                    mapRequest.put(arrSplitEqual[0], "");
+                }
+            }
+        }
+        return mapRequest;
+    }
+
+    /**
+     * 去掉url中的路径，留下请求参数部分
+     *
+     * @param strURL url地址
+     * @return url请求参数部分
+     */
+    private static String TruncateUrlPage(String strURL) {
+        String strAllParam = null;
+        String[] arrSplit = null;
+
+        strURL = strURL.trim().toLowerCase();
+
+        arrSplit = strURL.split("[?]");
+        if (strURL.length() > 1) {
+            if (arrSplit.length > 1) {
+                if (arrSplit[1] != null) {
+                    strAllParam = arrSplit[1];
+                }
+            }
+        }
+
+        return strAllParam;
+    }
+
+    /**
+     * 解析出url请求的路径，包括页面
+     * @param strURL url地址
+     * @return url路径
+     */
+    public static String UrlPage(String strURL)
+    {
+        String strPage=null;
+        String[] arrSplit=null;
+
+        strURL=strURL.trim().toLowerCase();
+
+        arrSplit=strURL.split("[?]");
+        if(strURL.length()>0)
+        {
+            if(arrSplit.length>1)
+            {
+                if(arrSplit[0]!=null)
+                {
+                    strPage=arrSplit[0];
+                }
+            }
+        }
+
+        return strPage;
+    }
+}
