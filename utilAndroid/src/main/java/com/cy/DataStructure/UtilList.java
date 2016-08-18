f5790386ddc86dfe69ba2a99eab1c4be3645dbb8
djun100 <djun100@qq.com>
2016-08-18 09:21:56 +0800

    a

--- utilAndroid/src/main/java/com/cy/DataStructure/UtilList.java
+++ utilAndroid/src/main/java/com/cy/DataStructure/UtilList.java
@@ -8,90 +8,99 @@ import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
-/**
- *  
- */  
 public class UtilList {
 
-	public  static <E> boolean notEmpty(List<E> list){
-		return list!=null && list.size()>0;
-	}
+    public static <E> boolean notEmpty(List<E> list) {
+        return list != null && list.size() > 0;
+    }
 
-	public static <E> List<E> convert_arrayToList(E[] array){
+    public static <E> List<E> convert_arrayToList(E[] array) {
 
-		List<E> userList = new ArrayList<E>();
+        List<E> userList = new ArrayList<E>();
 
-		if (array==null||array.length==0){
-			return userList;
-		}
+        if (array == null || array.length == 0) {
+            return userList;
+        }
 
-		Collections.addAll(userList, array);
-		return userList;
-	}
+        Collections.addAll(userList, array);
+        return userList;
+    }
 
-	public static <E> E[] convert_listToArray(List<E> list){
-		if (list==null || list.size()==0){
-			return null;
-		}
-		E[] array = (E[]) list.toArray();
-		return array;
-	}
+    public static <E> E[] convert_listToArray(List<E> list) {
+        if (list == null || list.size() == 0) {
+            return null;
+        }
+        E[] array = (E[]) list.toArray();
+        return array;
+    }
 
-	/**
-	 * 自定义排序，专门针对列表（List）中的数据进行排序；可按指定方法进行。
-	 * 目前实现对字符串（String）、日期（Date）、整型（Integer）等三种对象进行排序。
-	 * 对列表中的数据按指定字段进行排序。要求类必须有相关的方法返回字符串、整型、日期等值以进行比较。
-	 * @param list
-	 * @param method eg:getUsername
-	 * @param bigToLittle
-	 */
-	public  static <E> void sortByMethod(List<E> list, final String method, final boolean bigToLittle) {
-		Collections.sort(list, new Comparator<Object>() {
-			@SuppressWarnings("unchecked")
-			public int compare(Object arg1, Object arg2) {
-				int result = 0;
-				try {
-					Method m1 = ((E) arg1).getClass().getMethod(method, null);
-					Method m2 = ((E) arg2).getClass().getMethod(method, null);
-					Object obj1 = m1.invoke(((E)arg1), null);
-					Object obj2 = m2.invoke(((E)arg2), null);
-					if(obj1 instanceof String) {
-						// 字符串
-						result = obj1.toString().compareTo(obj2.toString());
-					}else if(obj1 instanceof Date) {
-						// 日期
-						long l = ((Date)obj1).getTime() - ((Date)obj2).getTime();
-						if(l > 0) {
-							result = 1;
-						}else if(l < 0) {
-							result = -1;
-						}else {
-							result = 0;
-						}
-					}else if(obj1 instanceof Integer) {
-						// 整型（Method的返回参数可以是int的，因为JDK1.5之后，Integer与int可以自动转换了）
-						result = (Integer)obj1 - (Integer)obj2;
-					}else {
-						// 目前尚不支持的对象，直接转换为String，然后比较，后果未知
-						result = obj1.toString().compareTo(obj2.toString());
+    /**
+     * based on http://www.cnblogs.com/nayitian/p/3322267.html
+     * <p>
+     * 要求 对列表（List）中的自定义对象，要求能够按照对象的属性（字段）进行排序（正序、倒序）。
+     * 如：用户对象（Member）有用户名（username）、级别（level）、出生日期（birthday）等字段，要求可以分别对它的三个字段进行排序。
+     * <p>
+     * 实现思路
+     * 对于自定义对象，可以在自定义对象中实现Comparable接口，然后再调用Collections.sort的方法实现排序，只能是针对一个属性（字段），维持一个顺序；要实多字段任意选择一个排序，同样需要通过调用Collections.sort(List list, Comparator<? super T> c)方法，传进一个Comparator来实现。
+     * <p>
+     * 为避免上述步骤中复杂且重复的代码，可以写一个通用的排序类，能够对自定义对象，针对不同的属性（字段），实现排序（正序、倒序）。
+     * <p>
+     * <p>
+     * 自定义排序，专门针对列表（List）中的数据进行排序；可按指定方法进行。
+     * 目前实现对字符串（String）、日期（Date）、整型（Integer）等三种对象进行排序。
+     * 对列表中的数据按指定字段进行排序。要求类必须有相关的方法返回字符串、整型、日期等值以进行比较。
+     *
+     * @param list
+     * @param method      eg:getUsername
+     * @param bigToLittle
+     */
+    public static <E> void sortByMethod(List<E> list, final String method, final boolean bigToLittle) {
+        Collections.sort(list, new Comparator<Object>() {
+            @SuppressWarnings("unchecked")
+            public int compare(Object arg1, Object arg2) {
+                int result = 0;
+                try {
+                    Method m1 = ((E) arg1).getClass().getMethod(method, null);
+                    Method m2 = ((E) arg2).getClass().getMethod(method, null);
+                    Object obj1 = m1.invoke(((E) arg1), null);
+                    Object obj2 = m2.invoke(((E) arg2), null);
+                    if (obj1 instanceof String) {
+                        // 字符串
+                        result = obj1.toString().compareTo(obj2.toString());
+                    } else if (obj1 instanceof Date) {
+                        // 日期
+                        long l = ((Date) obj1).getTime() - ((Date) obj2).getTime();
+                        if (l > 0) {
+                            result = 1;
+                        } else if (l < 0) {
+                            result = -1;
+                        } else {
+                            result = 0;
+                        }
+                    } else if (obj1 instanceof Integer) {
+                        // 整型（Method的返回参数可以是int的，因为JDK1.5之后，Integer与int可以自动转换了）
+                        result = (Integer) obj1 - (Integer) obj2;
+                    } else {
+                        // 目前尚不支持的对象，直接转换为String，然后比较，后果未知
+                        result = obj1.toString().compareTo(obj2.toString());
 
-						System.err.println("UtilSortList.sortByMethod方法接受到不可识别的对象类型，转换为字符串后比较返回...");
-					}
+                        System.err.println("UtilSortList.sortByMethod方法接受到不可识别的对象类型，转换为字符串后比较返回...");
+                    }
 
-					if (bigToLittle) {
-						// 倒序
-						result = -result;
-					}
-				} catch (NoSuchMethodException nsme) {
-					nsme.printStackTrace();
-				} catch (IllegalAccessException iae) {
-					iae.printStackTrace();
-				} catch (InvocationTargetException ite) {
-					ite.printStackTrace();
-				}
+                    if (bigToLittle) {
+                        // 倒序
+                        result = -result;
+                    }
+                } catch (NoSuchMethodException nsme) {
+                    nsme.printStackTrace();
+                } catch (IllegalAccessException iae) {
+                    iae.printStackTrace();
+                } catch (InvocationTargetException ite) {
+                    ite.printStackTrace();
+                }
 
-				return result;
-			}
-		});
-	}
+                return result;
+            }
+        });
+    }
 }  
