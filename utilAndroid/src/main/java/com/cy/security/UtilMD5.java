fc2db2724ae1a78fd970346998135bed34b47fd0
djun100 <djun100@qq.com>
2017-01-22 16:51:22 +0800

    add file md5 algorithm

--- utilAndroid/src/main/java/com/cy/security/UtilMD5.java
+++ utilAndroid/src/main/java/com/cy/security/UtilMD5.java
@@ -2,6 +2,8 @@ package com.cy.security;
 
 import com.cy.DataStructure.UtilsByte;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -44,5 +46,47 @@ public class UtilMD5 {
 		}
 		digest.update(s.getBytes());
 		return UtilsByte.byteArray2HexString(digest.digest());
+	}
+
+	/**http://blog.csdn.net/u012416914/article/details/50395508
+	 * @param file
+	 * @return
+	 */
+	public static String getFileMD5(File file) {
+		if (!file.isFile()) {
+			return null;
+		}
+		MessageDigest digest = null;
+		FileInputStream in = null;
+		byte buffer[] = new byte[1024];
+		int len;
+		try {
+			digest = MessageDigest.getInstance("MD5");
+			in = new FileInputStream(file);
+			while ((len = in.read(buffer, 0, 1024)) != -1) {
+				digest.update(buffer, 0, len);
+			}
+			in.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		return bytesToHexString(digest.digest());
+	}
+
+	public static String bytesToHexString(byte[] src) {
+		StringBuilder stringBuilder = new StringBuilder("");
+		if (src == null || src.length <= 0) {
+			return null;
+		}
+		for (int i = 0; i < src.length; i++) {
+			int v = src[i] & 0xFF;
+			String hv = Integer.toHexString(v);
+			if (hv.length() < 2) {
+				stringBuilder.append(0);
+			}
+			stringBuilder.append(hv);
+		}
+		return stringBuilder.toString();
 	}
 }
