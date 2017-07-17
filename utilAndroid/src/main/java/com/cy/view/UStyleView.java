35b7f472cec5d6e432cb9b400d3b6b1da2a28992
wangxuechao <djun100@qq.com>
2017-07-17 18:30:25 +0800

    增加样式化view工具类

---
+++ utilAndroid/src/main/java/com/cy/view/UStyleView.java
@@ -0,0 +1,255 @@
+package com.cy.view;
+
+import android.annotation.TargetApi;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.RippleDrawable;
+import android.graphics.drawable.StateListDrawable;
+import android.os.Build;
+import android.support.annotation.ColorInt;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * extract from https://github.com/H07000223/FlycoRoundView
+ * RippleDrawable 知识参考： http://www.jianshu.com/p/0ef14eda6064
+ * 使用方法：
+ new UStyleView(mbtn).setBackgroundColor(0xff22dd90)
+ .setStrokeColor(Color.GREEN)
+ .setStrokeWidth(1)
+ //.setBackgroundPressColor(0xffff0000)
+ .setRadiusHalfHeight(true)
+ .setRippleEnable(true)
+ .apply();
+
+ * Created by cy on 2017/7/17.
+ */
+
+public class UStyleView {
+
+    private View view;
+    private int cornerRadius;
+    private boolean isRadiusHalfHeight;
+    private int backgroundColor;
+    private int backgroundPressColor;
+    private int cornerRadius_TL;
+    private int cornerRadius_TR;
+    private int cornerRadius_BL;
+    private int cornerRadius_BR;
+    private int strokeWidth;
+    private int strokeColor;
+    private int strokePressColor;
+    private int textPressColor = Integer.MAX_VALUE;
+    private boolean isRippleEnable;
+    private float[] radiusArr = new float[8];
+    private GradientDrawable gd_background = new GradientDrawable();
+    private GradientDrawable gd_background_press = new GradientDrawable();
+
+    public UStyleView(View view) {
+        this.view = view;
+    }
+
+    public int getCornerRadius() {
+        return cornerRadius;
+    }
+
+    public UStyleView setCornerRadius(int cornerRadius) {
+        this.cornerRadius = cornerRadius;
+        return this;
+    }
+
+    public boolean isRadiusHalfHeight() {
+        return isRadiusHalfHeight;
+    }
+
+    public UStyleView setRadiusHalfHeight(boolean radiusHalfHeight) {
+        isRadiusHalfHeight = radiusHalfHeight;
+        return this;
+    }
+
+    public int getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public UStyleView setBackgroundColor(int backgroundColor) {
+        this.backgroundColor = backgroundColor;
+        return this;
+    }
+
+    public int getBackgroundPressColor() {
+        return backgroundPressColor;
+    }
+
+    public UStyleView setBackgroundPressColor(int backgroundPressColor) {
+        this.backgroundPressColor = backgroundPressColor;
+        return this;
+    }
+
+    public int getCornerRadius_TL() {
+        return cornerRadius_TL;
+    }
+
+    public UStyleView setCornerRadius_TL(int cornerRadius_TL) {
+        this.cornerRadius_TL = cornerRadius_TL;
+        return this;
+    }
+
+    public int getCornerRadius_TR() {
+        return cornerRadius_TR;
+    }
+
+    public UStyleView setCornerRadius_TR(int cornerRadius_TR) {
+        this.cornerRadius_TR = cornerRadius_TR;
+        return this;
+    }
+
+    public int getCornerRadius_BL() {
+        return cornerRadius_BL;
+    }
+
+    public UStyleView setCornerRadius_BL(int cornerRadius_BL) {
+        this.cornerRadius_BL = cornerRadius_BL;
+        return this;
+    }
+
+    public int getCornerRadius_BR() {
+        return cornerRadius_BR;
+    }
+
+    public UStyleView setCornerRadius_BR(int cornerRadius_BR) {
+        this.cornerRadius_BR = cornerRadius_BR;
+        return this;
+    }
+
+    public int getStrokeWidth() {
+        return strokeWidth;
+    }
+
+    public UStyleView setStrokeWidth(int strokeWidth) {
+        this.strokeWidth = strokeWidth;
+        return this;
+    }
+
+    public int getStrokeColor() {
+        return strokeColor;
+    }
+
+    public UStyleView setStrokeColor(int strokeColor) {
+        this.strokeColor = strokeColor;
+        return this;
+    }
+
+    public int getStrokePressColor() {
+        return strokePressColor;
+    }
+
+    public UStyleView setStrokePressColor(int strokePressColor) {
+        this.strokePressColor = strokePressColor;
+        return this;
+    }
+
+    public int getTextPressColor() {
+        return textPressColor;
+    }
+
+    public UStyleView setTextPressColor(@ColorInt int textPressColor) {
+        this.textPressColor = textPressColor;
+        return this;
+    }
+
+    public boolean isRippleEnable() {
+        return isRippleEnable;
+    }
+
+    public UStyleView setRippleEnable(boolean rippleEnable) {
+        isRippleEnable = rippleEnable;
+        return this;
+    }
+
+
+    private void fillGradientDrawable(GradientDrawable gd, int color, int strokeColor) {
+        gd.setColor(color);
+
+        if (cornerRadius_TL > 0 || cornerRadius_TR > 0 || cornerRadius_BR > 0 || cornerRadius_BL > 0) {
+            /**The corners are ordered top-left, top-right, bottom-right, bottom-left*/
+            radiusArr[0] = cornerRadius_TL;
+            radiusArr[1] = cornerRadius_TL;
+            radiusArr[2] = cornerRadius_TR;
+            radiusArr[3] = cornerRadius_TR;
+            radiusArr[4] = cornerRadius_BR;
+            radiusArr[5] = cornerRadius_BR;
+            radiusArr[6] = cornerRadius_BL;
+            radiusArr[7] = cornerRadius_BL;
+            gd.setCornerRadii(radiusArr);
+        } else if (cornerRadius > 0) {
+            gd.setCornerRadius(cornerRadius);
+        } else if (isRadiusHalfHeight) {
+            gd.setCornerRadius(view.getHeight() / 2);
+        }
+
+        gd.setStroke(strokeWidth, strokeColor);
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private ColorStateList getPressedColorSelector(int normalColor, int pressedColor) {
+        return new ColorStateList(
+                new int[][]{
+                        new int[]{android.R.attr.state_pressed},
+                        new int[]{android.R.attr.state_focused},
+                        new int[]{android.R.attr.state_activated},
+                        new int[]{}
+                },
+                new int[]{
+                        pressedColor,
+                        pressedColor,
+                        pressedColor,
+                        normalColor
+                }
+        );
+    }
+
+    public void apply() {
+        StateListDrawable bg = new StateListDrawable();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && isRippleEnable) {
+
+
+            fillGradientDrawable(gd_background, backgroundColor, strokeColor);
+            RippleDrawable rippleDrawable = new RippleDrawable(
+                    ColorStateList.valueOf(Color.GRAY),//灰色波纹
+//						getPressedColorSelector(backgroundColor, backgroundPressColor),
+                    gd_background,
+                    null);
+            view.setBackground(rippleDrawable);
+
+
+        } else {
+            fillGradientDrawable(gd_background, backgroundColor, strokeColor);
+            bg.addState(new int[]{-android.R.attr.state_pressed}, gd_background);
+            if (backgroundPressColor != Integer.MAX_VALUE || strokePressColor != Integer.MAX_VALUE) {
+                fillGradientDrawable(gd_background_press, backgroundPressColor == Integer.MAX_VALUE ? backgroundColor : backgroundPressColor,
+                        strokePressColor == Integer.MAX_VALUE ? strokeColor : strokePressColor);
+                bg.addState(new int[]{android.R.attr.state_pressed}, gd_background_press);
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {//16
+                view.setBackground(bg);
+            } else {
+                //noinspection deprecation
+                view.setBackgroundDrawable(bg);
+            }
+        }
+
+        if (view instanceof TextView) {
+            if (textPressColor != Integer.MAX_VALUE) {
+                ColorStateList textColors = ((TextView) view).getTextColors();
+//              Log.d("AAA", textColors.getColorForState(new int[]{-android.R.attr.state_pressed}, -1) + "");
+                ColorStateList colorStateList = new ColorStateList(
+                        new int[][]{new int[]{-android.R.attr.state_pressed}, new int[]{android.R.attr.state_pressed}},
+                        new int[]{textColors.getDefaultColor(), textPressColor});
+                ((TextView) view).setTextColor(colorStateList);
+            }
+        }
+    }
+}
