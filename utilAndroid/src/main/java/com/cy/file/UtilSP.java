9a26fb0253e05c3b733e949af7c693d2f48ddd83
wangxuechao <djun100@qq.com>
2019-11-14 15:00:37 +0800

    a

--- utilAndroid/src/main/java/com/cy/file/UtilSP.java
+++ utilAndroid/src/main/java/com/cy/file/UtilSP.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 import com.cy.app.UtilContext;
+import com.cy.io.KLog;
 
 /**
  * SharedPreferences的一个工具类，调用setParam就能保存String, Integer, Boolean, Float, Long类型的参数
@@ -16,12 +17,11 @@ public class UtilSP {
 	 * 保存在手机里面的文件名
 	 */
 	private static final String FILE_NAME = "share_date";
-	
-	
+
 	/**
 	 * 保存数据的方法，我们需要拿到保存数据的具体类型，然后根据类型调用不同的保存方法
 	 * @param key
-	 * @param object 
+	 * @param object
 	 */
 	public static void set(String key, Object object){
 		set(FILE_NAME, key, object);
@@ -29,62 +29,72 @@ public class UtilSP {
 	/**
 	 * 保存数据的方法，我们需要拿到保存数据的具体类型，然后根据类型调用不同的保存方法
 	 * @param key
-	 * @param object 
+	 * @param value
 	 */
-	public static void set(String fileName, String key, Object object){
-		String type = object.getClass().getSimpleName();
+	public static <T> void set(String fileName, String key, T value){
+
 		SharedPreferences sp = UtilContext.getContext().getSharedPreferences(fileName, Context.MODE_PRIVATE);
 		SharedPreferences.Editor editor = sp.edit();
-		
-		if("String".equals(type)){
-			editor.putString(key, (String)object);
+
+		if(value instanceof String){
+			editor.putString(key, (String)value);
 		}
-		else if("Integer".equals(type)){
-			editor.putInt(key, (Integer)object);
+		else if(value instanceof Integer){
+			editor.putInt(key, (Integer)value);
 		}
-		else if("Boolean".equals(type)){
-			editor.putBoolean(key, (Boolean)object);
+		else if(value instanceof Boolean){
+			editor.putBoolean(key, (Boolean)value);
 		}
-		else if("Float".equals(type)){
-			editor.putFloat(key, (Float)object);
+		else if(value instanceof Float){
+			editor.putFloat(key, (Float)value);
 		}
-		else if("Long".equals(type)){
-			editor.putLong(key, (Long)object);
+		else if(value instanceof Long){
+			editor.putLong(key, (Long)value);
 		}
-		
+		else if (value == null){
+			editor.putString(key, null);
+		}
+
 		editor.commit();
+//		UtilProperties.write(key,value+"");
 	}
-	
-	
+
+
 	/**
 	 * 得到保存数据的方法，我们根据默认值得到保存的数据的具体类型，然后调用相对于的方法获取值
 	 * @param key
-	 * @param defaultObject
+	 * @param defaultValue
 	 * @return
 	 */
-	public static <T> T get(String fileName, String key, T defaultObject){
-		String type;
-		if (defaultObject == null) {
-			type = "String";
-		} else {
-			type = defaultObject.getClass().getSimpleName();
-		}
+	public static <T> T get(String fileName, String key, T defaultValue){
 		SharedPreferences sp = UtilContext.getContext().getSharedPreferences(fileName, Context.MODE_PRIVATE);
 
-		if ("String".equals(type)) {
-			return (T) sp.getString(key, (String) defaultObject);
-		} else if ("Integer".equals(type)) {
-			int result = sp.getInt(key, (Integer) defaultObject);
-			return (T) new Integer(result);
-		} else if ("Boolean".equals(type)) {
-			return (T) new Boolean(sp.getBoolean(key, (Boolean) defaultObject));
-		} else if ("Float".equals(type)) {
-			return (T) new Float(sp.getFloat(key, (Float) defaultObject));
-		} else if ("Long".equals(type)) {
-			return (T) new Long(sp.getLong(key, (Long) defaultObject));
-		}
-		
-		return defaultObject;
+		if (sp.contains(key)) {
+			if (defaultValue instanceof String) {
+				return (T) sp.getString(key, (String) defaultValue);
+			}
+			else if (defaultValue instanceof Integer) {
+				int result = sp.getInt(key, (Integer) defaultValue);
+				return (T) new Integer(result);
+			}
+			else if (defaultValue instanceof Boolean) {
+				return (T) new Boolean(sp.getBoolean(key, (Boolean) defaultValue));
+			}
+			else if (defaultValue instanceof Float) {
+				return (T) new Float(sp.getFloat(key, (Float) defaultValue));
+			}
+			else if (defaultValue instanceof Long) {
+				return (T) new Long(sp.getLong(key, (Long) defaultValue));
+			}
+			else if (defaultValue == null){
+				return (T) sp.getString(key, null);
+			}
+		}/*else {
+			T result= UtilProperties.read(key,defaultValue);
+			KLog.i("从Properties取出值：" + result);
+			return result;
+		}*/
+		return defaultValue;
 	}
 	/**
 	 * 得到保存数据的方法，我们根据默认值得到保存的数据的具体类型，然后调用相对于的方法获取值
@@ -95,4 +105,13 @@ public class UtilSP {
 	public static <T> T get( String key, T defaultObject){
 		return get(FILE_NAME, key, defaultObject);
 	}
+
+	public static void main(String[] args) {
+		set("keySp","prop");
+		String key1 = get("keySp",null);
+		String key2 = get("keySp","");
+//        String key3=getParam("key",1);  cannot compile
+		KLog.i(String.format("key1:%s,key2:%s",key1,key2));
+	}
 }
+
