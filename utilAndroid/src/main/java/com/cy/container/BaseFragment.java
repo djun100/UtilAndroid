4844938863990b1acbfc73bcb097df8999ed7005
wangxuechao <djun100@qq.com>
2020-04-05 17:45:04 +0800

    增加生命周期日志

--- utilAndroid/src/main/java/com/cy/container/BaseFragment.java
+++ utilAndroid/src/main/java/com/cy/container/BaseFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 
 import com.cy.io.Log;
@@ -63,6 +66,19 @@ public abstract class BaseFragment extends Fragment {
         if (savedInstanceState!=null){
             Log.i("savedInstanceState:"+Log.bundle2String(savedInstanceState));
         }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.i();
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Log.i();
     }
 
     @Override
