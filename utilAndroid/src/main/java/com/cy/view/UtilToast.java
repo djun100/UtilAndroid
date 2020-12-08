e2e051a57c8aa4b9157c1d3effb7112b06e08132
wangxuechao <djun100@qq.com>
2020-12-08 10:21:59 +0800

    a

--- utilAndroid/src/main/java/com/cy/view/UtilToast.java
+++ utilAndroid/src/main/java/com/cy/view/UtilToast.java
@@ -1,20 +1,37 @@
 package com.cy.view;
 
-import android.os.Looper;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.StringRes;
 import android.widget.Toast;
+
 
 import com.cy.app.UtilContext;
 import com.cy.communication.UtilThread;
 
+import java.lang.reflect.Field;
+
+
 public class UtilToast {
+
+    private static Field sField_TN;
+    private static Field sField_TN_Handler;
+    static {
+        try {
+            sField_TN = Toast.class.getDeclaredField("mTN");
+            sField_TN.setAccessible(true);
+            sField_TN_Handler = sField_TN.getType().getDeclaredField("mHandler");
+            sField_TN_Handler.setAccessible(true);
+        } catch (Exception e) {}
+    }
 
     public static void showShort(String text){
         show(text,Toast.LENGTH_SHORT);
     }
 
     public static void showShort(@StringRes int textRes){
-        show(textRes,Toast.LENGTH_SHORT);
+        show(UtilContext.getContext().getString(textRes),Toast.LENGTH_SHORT);
     }
 
     public static void showLong(String text){
@@ -22,31 +39,63 @@ public class UtilToast {
     }
 
     public static void showLong(@StringRes int textRes){
-        show(textRes,Toast.LENGTH_LONG);
-    }
-
-
-
-    private static void show(@StringRes int textRes, int timeLength){
-        show(UtilContext.getContext().getString(textRes),timeLength);
+        show(UtilContext.getContext().getString(textRes),Toast.LENGTH_LONG);
     }
 
     /**在非UI线程显示toast，如果使用Looper.prepare()...Looper.loop()，因loop()内部有死循环，
      * 所以toast后面不能有代码;
      * 所以非UI线程可以通过looper显示toast，但不建议使用，应转到UI线程去toast
      * @param text
-     * @param timeLength
+     * @param duration
      */
-    private static void show(final String text, final int timeLength){
+    private static void show(final String text, final int duration){
         if (UtilThread.isMainThread()){
-            Toast.makeText(UtilContext.getContext(), text, timeLength).show();
+            _show(text, duration);
         }else {
             UtilThread.runOnUIThread(new Runnable() {
                 @Override
                 public void run() {
-                    Toast.makeText(UtilContext.getContext(), text, timeLength).show();
+                    _show(text, duration);
                 }
             });
+        }
+    }
+
+    private static void _show(final String text, final  int duration) {
+        Toast toast = Toast.makeText(UtilContext.getContext(), text, duration);
+        if(Build.VERSION.SDK_INT < 26) { //Android8.0以前
+            hook(toast);
+        }
+        toast.show();
+    }
+
+    private static void hook(Toast toast) {
+        try {
+            Object tn = sField_TN.get(toast);
+            Handler preHandler = (Handler)sField_TN_Handler.get(tn);
+            sField_TN_Handler.set(tn,new SafelyHandlerWrapper(preHandler));
+        } catch (Exception e) {}
+    }
+
+
+    private static class SafelyHandlerWrapper extends Handler {
+
+        private final Handler impl;
+
+        public SafelyHandlerWrapper(Handler impl) {
+            this.impl = impl;
+        }
+
+        @Override
+        public void dispatchMessage(Message msg) {
+            try {
+                super.dispatchMessage(msg);
+            } catch (Exception e) {}
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            impl.handleMessage(msg);//需要委托给原Handler执行
         }
     }
 }
